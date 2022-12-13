ece0a91be3fd154dbb3e776048e951fbb0a86111
dbslm31 <104209863+dbslm31@users.noreply.github.com>
2022-12-13 17:22:40 +0100

    react-router-setup

--- kasa-app/src/index.js
+++ kasa-app/src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './component/App';
-import './styles/Index.css'
+import './styles/Index.css';
+import { Router } from "react-router-dom"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Router />
     <App />
   </React.StrictMode>
 );
